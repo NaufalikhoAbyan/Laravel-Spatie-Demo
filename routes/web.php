6a56d9d021a9607758da55d361ccdb6534083c55
NaufalikhoAbyan <apradana448@gmail.com>
2024-07-29 10:32:00 +0700

    feat: Set up Spatie permission

--- routes/web.php
+++ routes/web.php
@@ -3,13 +3,15 @@
 use App\Http\Controllers\Auth\AuthController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return inertia('IndexView');
-})->name('home')->middleware('auth');
+Route::middleware(['auth'])->group(function () {
+    Route::get('/', function () {
+        return inertia('IndexView');
+    })->name('home');
 
-Route::get('/about', function () {
-    return inertia('AboutView');
-})->name('about')->middleware('auth');
+    Route::get('/about', function () {
+        return inertia('AboutView');
+    })->name('about');
+});
 
 Route::get('/login', [AuthController::class, 'login'])->name('login');
 Route::post('/login', [AuthController::class, 'authenticate'])->name('authenticate');
